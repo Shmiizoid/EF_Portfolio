4eeadb9a514d32498e87378f70c51622ff6d909b
Shmiizoid <eafuentes949@gmail.com>
2023-06-11 14:48:08 -0700

    undo

--- landing.js
+++ landing.js
@@ -1,6 +1,7 @@
 
 //Click arrow function Landing page_________________________________________
 const pageLink = document.getElementById("landing");
+console.log(pageLink)
     pageLink.addEventListener('click', () => goToPage ('home.html'));
 
     const goToPage = (page) => {
