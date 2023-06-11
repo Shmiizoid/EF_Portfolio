a2e7dc13a1b1411f0279d44256556e504a0f36c8
Shmiizoid <eafuentes949@gmail.com>
2023-06-11 14:44:06 -0700

    error on landing

--- landing.js
+++ landing.js
@@ -1,7 +1,6 @@
 
 //Click arrow function Landing page_________________________________________
 const pageLink = document.getElementById("landing");
-console.log(pageLink)
     pageLink.addEventListener('click', () => goToPage ('home.html'));
 
     const goToPage = (page) => {
