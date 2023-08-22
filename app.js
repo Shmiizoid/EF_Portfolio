ccc10c327515c98454262bd4ca2d3403bd82969c
Shmiizoid <eafuentes949@gmail.com>
2023-08-21 20:28:02 -0700

    updated resume for download

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ function sendMessage(event) {
   event.preventDefault();
   alert('Message sent!');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const next = document.querySelector('.next');
+  const prev = document.querySelector('.prev');
+  const images = document.getElementsByClassName('images');
+  let currentImgIndex = 0;
+
+  next.addEventListener('click', () => {
+    let previousImgIndex = currentImgIndex;
+    currentImgIndex++;
+
+    if (currentImgIndex >= images.length) {
+      currentImgIndex = 0;
+    }
+
+    images[previousImgIndex].style.display = 'none';
+    images[currentImgIndex].style.display = 'block';
+  });
+
+  prev.addEventListener('click', () => {
+    let previousImgIndex = currentImgIndex;
+    currentImgIndex--;
+
+    if (currentImgIndex < 0) {
+      currentImgIndex = images.length - 1;
+    }
+
+    images[previousImgIndex].style.display = 'none';
+    images[currentImgIndex].style.display = 'block';
+  });
+
+});
